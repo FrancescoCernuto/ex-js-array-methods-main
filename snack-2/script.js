babb6f87f8f203a1c80bce7b16d47ae3910df371
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-05 16:30:47 +0100

    fix-snack2

--- snack-2/script.js
+++ snack-2/script.js
@@ -7,6 +7,6 @@ const people = [
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
 
-people.forEach((people) => {
+people.forEach((person) => {
   console.log(person.name);
 });
